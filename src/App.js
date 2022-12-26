5d75d19931e6febec340525838ba346633e7dac6
Pusaka Manggala <pusakamanggala@gmail.com>
2022-12-26 18:12:09 +0700

    update : file locations

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import SplashScreens from './pages/SplashScreens';
+
+const App = () => {
+  return (
+    <View>
+      <SplashScreens />
+    </View>
+  );
+};
+export default App;
