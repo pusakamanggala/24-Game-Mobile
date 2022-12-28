455350e31913c079737a7e81104adcb102d2ac7f
Pusaka Manggala <pusakamanggala@gmail.com>
2022-12-28 18:52:06 +0700

    add : main menu

---
+++ src/pages/MainMenu.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import MainMenuButton from '../components/MainMenuButton';
+import {View, Text, Image, ImageBackground} from 'react-native';
+
+const MainMenu = () => {
+  return (
+    <View
+      style={{
+        height: '100%',
+        backgroundColor: '#13C8EF',
+      }}>
+      <View
+        style={{
+          height: '60%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Image source={require('../img/appLogo.png')} />
+      </View>
+
+      <View style={{height: '40%'}}>
+        <ImageBackground
+          source={require('../img/waveBackground.png')}
+          style={{height: '100%'}}>
+          <MainMenuButton />
+        </ImageBackground>
+      </View>
+    </View>
+  );
+};
+
+export default MainMenu;
