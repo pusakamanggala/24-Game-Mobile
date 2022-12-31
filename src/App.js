72208c74719b6e557aa3911938f1f99e2a7009f1
Pusaka Manggala <pusakamanggala@gmail.com>
2022-12-31 19:34:13 +0700

    add : stack navigator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import {NavigationContainer, StackActions} from '@react-navigation/native';
 import React from 'react';
-import {View, Text} from 'react-native';
+import Gameplay from './pages/Gameplay';
+import MainMenu from './pages/MainMenu';
 import SplashScreen from './pages/SplashScreen';
+import 'react-native-gesture-handler';
+import {createStackNavigator} from '@react-navigation/stack';
+import SelectMode from './pages/SelectMode';
+import {CardProvider} from './context/CardContext';
 
 const App = () => {
+  const Stack = createStackNavigator();
   return (
-    <View>
-      <SplashScreen />
-    </View>
+   
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen name="SplashScreen" component={SplashScreen} />
+          <Stack.Screen name="MainMenu" component={MainMenu} />
+          <Stack.Screen name="Gameplay" component={Gameplay} />
+          <Stack.Screen name="SelectMode" component={SelectMode} />
+        </Stack.Navigator>
+      </NavigationContainer>
+   
   );
 };
 export default App;
