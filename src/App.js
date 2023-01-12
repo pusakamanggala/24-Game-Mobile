c296d32258b8eb8a31885721b552539c3afb3057
Pusaka Manggala <pusakamanggala@gmail.com>
2023-01-12 15:26:21 +0700

    delete : keyboard component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {NavigationContainer, StackActions} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import Gameplay from './pages/Gameplay';
 import MainMenu from './pages/MainMenu';
@@ -6,24 +6,23 @@ import SplashScreen from './pages/SplashScreen';
 import 'react-native-gesture-handler';
 import {createStackNavigator} from '@react-navigation/stack';
 import SelectMode from './pages/SelectMode';
-import {CardProvider} from './context/CardContext';
+import Tutorial from './pages/Tutorial';
 
 const App = () => {
   const Stack = createStackNavigator();
   return (
-   
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-          }}>
-          <Stack.Screen name="SplashScreen" component={SplashScreen} />
-          <Stack.Screen name="MainMenu" component={MainMenu} />
-          <Stack.Screen name="Gameplay" component={Gameplay} />
-          <Stack.Screen name="SelectMode" component={SelectMode} />
-        </Stack.Navigator>
-      </NavigationContainer>
-   
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="SplashScreen" component={SplashScreen} />
+        <Stack.Screen name="MainMenu" component={MainMenu} />
+        <Stack.Screen name="Gameplay" component={Gameplay} />
+        <Stack.Screen name="SelectMode" component={SelectMode} />
+        <Stack.Screen name="Tutorial" component={Tutorial} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 export default App;
